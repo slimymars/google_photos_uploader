f0581890f1bad62cc3b8803444d14f22b0409e49
slimymars <slimymars@gmail.com>
2017-02-11 21:45:55 +0900

    元にしたファイルのコピーライト文を削除

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 // The onClicked callback function.
 function onClickHandler(info, tab) {
   // http://stackoverflow.com/questions/8262266/xmlhttprequest-multipart-related-post-with-xml-and-image-as-payload
